9849f4523124ebd24527871b03abb1e908bb35b6
Chiranth Gowda <97089430+ChiranthGowdaC@users.noreply.github.com>
2022-02-24 10:42:44 +0000

    p4 final.c

--- p4final.c
+++ p4final.c
@@ -3,24 +3,30 @@ int input()
 {
   int a;
   printf("enter a number\n");
-  scaf("%d",&a);
+  scanf("%d",&a);
   return a;
   
 }
-int find_fabo(int n)
+int  find_fabo(int n)
 {
-  int sum=0;
+  int a=0,b=1,c=0;
   for(int i=0;i<n;i++)
     {
-      
-    }
-  
+      c=a;
+      a=b;
+      b=c+a;  
+    } 
+  return c;
+ 
 }
-void output()
-{
+ void output(int n,int fibo)
+{ printf(" nth  term   is %d\n",fibo);
   
 }
 int main()
 {
-  
+  int a=input();
+  int b=find_fabo(a);
+  output(a,b);
+  return 0;
 }
